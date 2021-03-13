26d2a69f56daa4c80614ebfef38378998350c950
hilmyghozy <hilmyghozy11@gmail.com>
2021-03-13 12:04:17 +0700

    conversation screen

---
+++ Qiscus/ChatTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ChatTableViewCell.swift
+//  Qiscus
+//
+//  Created by hilmy ghozy on 13/03/21.
+//
+
+import UIKit
+
+class ChatTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
